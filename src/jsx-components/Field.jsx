d9583e334529e90aa5ae2a5a27967d1f5c9615da
ArabuliGiorgi <qartveli.giorgi.2003@gmail.com>
2024-10-21 23:53:16 +0400

    one more step

--- src/jsx-components/Field.jsx
+++ src/jsx-components/Field.jsx
@@ -1,32 +1,43 @@
 import './Field.css'
 import styled from 'styled-components'
-import { useState } from 'react';
+import errorImage from '/images/icon-error.svg'
+import { useState, useEffect } from 'react';
 
 export default function Field({placeholder, clicked}){
-    const [inputName, setInputName] = useState('normal');
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
     const [value, setValue] = useState('');
     const getValue = (event) => {
         setValue(event.target.value);
     }
-    if(clicked && value === ''){
-        setInputName('error');
-    }
+    useEffect(() => {
+        if (clicked && value === '') {
+            setIsError(true);
+        } else {
+            setIsError(false);
+        }
+    }, [clicked, value]);
 
     return(
         <>
-            <InputField 
-            type="text" 
-            placeholder={placeholder}
-            onChange={getValue}
-            className={inputName}
-            />
+            <div className='flex items-center'>
+                <InputField 
+                type="text" 
+                placeholder={placeholder}
+                onChange={getValue}
+                argument={isError}
+                />
+                <img src={errorImage} alt="error" className={`ml-[-53px] ${isError ? 'visible' : 'invisible'}`}/>
+            </div>
+            <h1 className={`${isError ? 'block' : 'hidden'}`} id='error-message'>{errorMessage}</h1>
         </>
     )
 }
 
 const InputField = styled.input`
-    border: 1px solid #DEDEDE;
+    border: ${(props) => props.argument ? '2px solid #FF7979' : '1px solid #DEDEDE'};
     border-radius: 5px;
     width: 100%;
     height: 56px;
+    padding-left: 20px;
 `;
