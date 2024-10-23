8b99f5f6666cafdd90433c9bde0f06734687fa3a
ArabuliGiorgi <qartveli.giorgi.2003@gmail.com>
2024-10-23 23:02:40 +0400

    refactor: make code cleaner & improve functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,115 @@
-import './App.css'
-import { useState } from 'react';
-import { Container1, Container2 } from './styled-components/Containers';
-import Field from './jsx-components/Field';
+import "./App.css";
+import { useState } from "react";
+import { Container1, Container2 } from "./styled-components/Containers";
+import Field from "./jsx-components/Field";
 
 function App() {
-  const [clicked, setClicked] = useState(false);
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    clickNumber: 0
+  });
+  function validate() {
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    let newErrors = { ...errors };
+    if (values.firstName === "") {
+      newErrors.firstName = "First Name cannot be empty";
+    } else {
+      newErrors.firstName = "";
+    }
+    if (values.lastName === "") {
+      newErrors.lastName = "Last Name cannot be empty";
+    } else {
+      newErrors.lastName = "";
+    }
+    if (values.email === "") {
+      newErrors.email = "Email cannot be empty";
+    } else if(!regex.test(values.email.trim())){
+      newErrors.email = "Looks like this is not an email";
+    }else{
+      newErrors.email = "";
+    }
+    if (values.password === "") {
+      newErrors.password = "Password cannot be empty";
+    } else if(values.password.length < 6){
+      newErrors.password = "Password is too short";
+    } else {
+      newErrors.password = "";
+    }
+
+    setErrors(newErrors);
+  }
 
   return (
     <main>
       <Container1>
         <h1>Learn to code by watching others</h1>
-        <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+        <p>
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
       </Container1>
       <Container2>
         <div className="plan">
-          <h1 className='font-bold text-[15px] leading-[26px] tracking-[0.27px] text-center text-white'>
-          Try it free 7 days <span className='font-normal'>then $20/mo. thereafter</span></h1>
+          <h1 className="font-bold text-[15px] leading-[26px] tracking-[0.27px] text-center text-white">
+            Try it free 7 days{" "}
+            <span className="font-normal">then $20/mo. thereafter</span>
+          </h1>
         </div>
         <div className="form">
-          <Field placeholder={'First Name'} clicked={clicked}></Field>
-          <Field placeholder={'Last Name'} clicked={clicked}></Field>
-          <Field placeholder={'Email Address'} clicked={clicked}></Field>
-          <Field placeholder={'Password'} clicked={clicked}></Field>
-          <button className='submit-btn' onClick={() => setClicked(true)}><h1>CLAIM YOUR FREE TRIAL</h1></button>
-          <h1 className='termsAndServices'>By clicking the button, you are agreeing to our <span className='text-[#FF7979] font-[700]'>Terms and Services</span></h1>
+          <Field
+            placeholder={"First Name"}
+            name="firstName"
+            setValues={setValues}
+            errors={errors}
+          ></Field>
+          <Field
+            placeholder={"Last Name"}
+            name="lastName"
+            setValues={setValues}
+            errors={errors}
+          ></Field>
+          <Field
+            placeholder={"Email Address"}
+            name="email"
+            setValues={setValues}
+            errors={errors}
+          ></Field>
+          <Field
+            placeholder={"Password"}
+            name="password"
+            setValues={setValues}
+            errors={errors}
+          ></Field>
+          <button className="submit-btn" onClick={() => {
+              validate();
+              setErrors(preValues => ({
+                ...preValues,
+                clickNumber: preValues.clickNumber + 1
+              }));
+            }}>
+            <h1>CLAIM YOUR FREE TRIAL</h1>
+          </button>
+          <h1 className="termsAndServices">
+            By clicking the button, you are agreeing to our{" "}
+            <span className="text-[#FF7979] font-[700]">
+              Terms and Services
+            </span>
+          </h1>
         </div>
       </Container2>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/jsx-components/Field.jsx
+++ src/jsx-components/Field.jsx
@@ -1,64 +1,66 @@
-import './Field.css'
-import styled from 'styled-components'
-import errorImage from '/images/icon-error.svg'
-import { useState, useEffect } from 'react';
+import "./Field.css";
+import styled from "styled-components";
+import errorImage from "/images/icon-error.svg";
+import { useState, useEffect } from "react";
 
-export default function Field({placeholder, clicked}){
-    const [isError, setIsError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [value, setValue] = useState('');
-    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    const getValue = (event) => {
-        setValue(event.target.value);
+export default function Field({ placeholder, setValues, errors, name }) {
+  const [isError, setIsError] = useState(false);
+  const getValue = (event) => {
+    setValues(prevValues => ({
+        ...prevValues,
+        [name]: event.target.value,
+    }));
+  };
+
+  useEffect(() => {
+    if(errors[name] != ""){
+        setIsError(true);
+    }else{
+        setIsError(false);
     }
-    useEffect(() => {
-        if(clicked){
-            if (value === '') {
-                setErrorMessage(`${placeholder} cannot be empty`);
-                setIsError(true);
-            } else {
-                setIsError(false);
-                if(placeholder == "Email Address" && !regex.test(value.trim())){
-                    setIsError(true);
-                    setErrorMessage('Looks like this is not an email');
-                }
-                if(placeholder == "Password" && value.length < 6){
-                    setIsError(true);
-                    setErrorMessage('Password is too short');
-                }
-            }
-        }
-    }, [clicked, value]);
+  }, [errors.clickNumber]);
 
-    return(
-        <div className='mb-[16px] lg:mb-[20px]'>
-            <div className='flex items-center'>
-                <InputField 
-                type={`${placeholder == "Password" ? 'password' : 'text'}`}
-                placeholder={isError ? '' : placeholder}
-                onChange={getValue}
-                argument={`${isError}`}
-                className='InputField'
-                />
-                <img src={errorImage} alt="error" className={`ml-[-53px] ${isError ? 'visible' : 'invisible'}`}/>
-            </div>
-            <h1 className={`${isError ? 'block' : 'hidden'}`} id='error-message'>{errorMessage}</h1>
-        </div>
-    )
+  return (
+    <div className="mb-[16px] lg:mb-[20px]">
+      <div className="flex items-center">
+        <InputField
+          type={`${placeholder == "Password" ? "password" : "text"}`}
+          placeholder={isError ? "" : placeholder}
+          onChange={(event) => {
+            getValue(event);
+            if(isError){
+                setIsError(false);
+            }
+          }}
+          argument={`${isError}`}
+          className="InputField"
+        />
+        <img
+          src={errorImage}
+          alt="error"
+          className={`ml-[-53px] ${isError ? "visible" : "invisible"}`}
+        />
+      </div>
+      <h1 className={`${isError ? "block" : "hidden"}`} id="error-message">
+        {errors[name]}
+      </h1>
+    </div>
+  );
 }
 
 const InputField = styled.input`
-    border: ${(props) => props.argument == 'true' ? '2px solid #FF7979' : '1px solid #DEDEDE'};
-    border-radius: 5px;
-    width: 100%;
-    height: 56px;
-    padding-left: 20px;
+  border: ${(props) =>
+    props.argument == "true" ? "2px solid #FF7979" : "1px solid #DEDEDE"};
+  border-radius: 5px;
+  width: 100%;
+  height: 56px;
+  padding-left: 20px;
 
-    font-size: 14px;
-    font-weight: 600;
-    line-height: 26px;
-    letter-spacing: 0.25px;
-    text-align: left;
-    color: ${(props) => props.argument == 'true' ? '#FF7979' : '#3D3B48'};
-    caret-color: #5E54A4;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 26px;
+  letter-spacing: 0.25px;
+  text-align: left;
+  color: ${(props) => (props.argument == "true" ? "#FF7979" : "#3D3B48")};
+  caret-color: #5e54a4;
 `;
